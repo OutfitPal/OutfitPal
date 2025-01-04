d48101decc1266be5fe1a49afa7a39e36ec4b536
kmb21 <113995857+kmb21@users.noreply.github.com>
2025-01-04 12:42:00 -0500

    added cloth manager

--- OutfitPal/OutfitPal/Core/Clothes/ClothManager.swift
+++ OutfitPal/OutfitPal/Core/Clothes/ClothManager.swift
@@ -6,3 +6,77 @@
 //
 
 import Foundation
+import Foundation
+import FirebaseFirestore
+import SwiftUICore
+
+
+
+final class ClothManager: ObservableObject {
+    
+    @EnvironmentObject var authManager: AuthManager
+    private let db = Firestore.firestore()
+    
+
+
+
+    static let shared = ClothManager()
+    
+    private init() {}
+
+
+ 
+    func addOutfit(for date: Date, outfit: String) {
+
+    }
+
+    func getCloth(for date: Date, completion: @escaping (String?) -> Void) {
+
+    }
+
+//    /// Fetches all stored outfits from Firestore.
+//    func getAllClothes() {
+//        db.collection("outfits").getDocuments { snapshot, error in
+//            if let error = error {
+//                print("Error fetching outfits: \(error.localizedDescription)")
+//                return
+//            }
+//
+//            DispatchQueue.main.async {
+//                self.outfits.removeAll()
+//            }
+//
+//            snapshot?.documents.forEach { document in
+//                if let data = document.data(),
+//                   let dateString = data["date"] as? String,
+//                   let outfit = data["outfit"] as? String,
+//                   let date = self.dateFromFormattedString(dateString) {
+//                    DispatchQueue.main.async {
+//                        self.outfits[date] = outfit
+//                    }
+//                }
+//            }
+//        }
+//    }
+
+
+    func removeCloth(for date: Date) {
+
+    }
+
+    // MARK: - Date Helpers
+
+ 
+    private func formattedDate(_ date: Date) -> String {
+        let formatter = DateFormatter()
+        formatter.dateFormat = "yyyy-MM-dd"
+        return formatter.string(from: date)
+    }
+
+   
+    private func dateFromFormattedString(_ dateString: String) -> Date? {
+        let formatter = DateFormatter()
+        formatter.dateFormat = "yyyy-MM-dd"
+        return formatter.date(from: dateString)
+    }
+}
